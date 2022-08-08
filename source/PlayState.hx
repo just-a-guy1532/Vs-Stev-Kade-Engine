cae67b1f9b09df59fa650a8c4c4c77b8f9f05f96
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-08-08 18:58:23 +0300

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -664,32 +664,16 @@ class PlayState extends MusicBeatState
 								add(waveSpriteFG);
 						*/
 			}
-			case 'stage':
+			case 'lol':
 				{
 						defaultCamZoom = 0.9;
 						curStage = 'stage';
-						var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image('stageback'));
+						var bg:FlxSprite = new FlxSprite(-300, -320).loadGraphic(Paths.image('stageimages/urghh'));
 						bg.antialiasing = true;
-						bg.scrollFactor.set(0.9, 0.9);
+						bg.scrollFactor.set(1, 1);
 						bg.active = false;
+					  bg.scale.set(1.3, 1.3);
 						add(bg);
-	
-						var stageFront:FlxSprite = new FlxSprite(-650, 600).loadGraphic(Paths.image('stagefront'));
-						stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
-						stageFront.updateHitbox();
-						stageFront.antialiasing = true;
-						stageFront.scrollFactor.set(0.9, 0.9);
-						stageFront.active = false;
-						add(stageFront);
-	
-						var stageCurtains:FlxSprite = new FlxSprite(-500, -300).loadGraphic(Paths.image('stagecurtains'));
-						stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
-						stageCurtains.updateHitbox();
-						stageCurtains.antialiasing = true;
-						stageCurtains.scrollFactor.set(1.3, 1.3);
-						stageCurtains.active = false;
-	
-						add(stageCurtains);
 				}
 			default:
 			{
