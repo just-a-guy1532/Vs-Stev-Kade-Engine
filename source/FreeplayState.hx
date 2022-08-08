dfe65e2b8e517db370ad7686953adba199498d94
VegethYT <84522055+VegethYT@users.noreply.github.com>
2022-08-09 00:40:27 +0200

    it's only normal now

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -235,9 +235,9 @@ class FreeplayState extends MusicBeatState
 		curDifficulty += change;
 
 		if (curDifficulty < 0)
-			curDifficulty = 2;
+			curDifficulty = 1;
 		if (curDifficulty > 2)
-			curDifficulty = 0;
+			curDifficulty = 1;
 
 		#if !switch
 		intendedScore = Highscore.getScore(songs[curSelected].songName, curDifficulty);
